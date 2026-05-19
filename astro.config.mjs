c2a90be86e205bfb276809844ca53cb0513b5c4d
jaram <jaramia96@gmail.com>
2026-05-19 09:25:29 +0200

    chore: clean unused project files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
-import keystatic from '@keystatic/astro';
-import react from '@astrojs/react';
 
 export default defineConfig({
-  output: 'hybrid',
-  adapter: netlify(),
   site: 'https://artassenx.fr',
   integrations: [
     tailwind(),
-    react(),
-    keystatic(),
   ],
 });
